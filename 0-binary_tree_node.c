ab38df4773d2aea43cd0fbbe0dc90b7b56ad48df
Angie Pérez <66028978+xioperez01@users.noreply.github.com>
2020-11-25 20:22:45 -0500

    Update 0-binary_tree_node.c

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -17,8 +17,5 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 
 	new_node->parent = parent;
 	new_node->n = value;
-	new_node->left = NULL;
-	new_node->right = NULL;
-
 	return (new_node);
 }
